3bb4b0f8f50c171259932fbb018472a1081e9bcb
kkahlua <csw93083@naver.com>
2022-05-11 10:23:18 +0900

    글 수정할 때 이전 이미지 삭제기능 추가

--- cctv_app/models.py
+++ cctv_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import sys, os
 
 class Board(models.Model):
     author = models.CharField(max_length=10, null=False)
@@ -10,3 +11,23 @@ class Board(models.Model):
 
     def __str__(self):
         return str(self.title)
+    
+    def save(self, *args, **kwargs):
+        mod = getattr(sys.modules[__name__], self._meta.object_name)
+
+        try:
+            obj = mod.objects.get(id=self.id)
+        except:
+            obj = None
+        else:
+            for field in obj._meta.fields:
+                field_type = field.get_internal_type()
+        if field_type == 'FileField' or field_type == 'ImageField':
+            origin_file = getattr(obj, field.name)
+            new_file = getattr(self, field.name)
+            if origin_file != new_file:
+                path = '.' + origin_file.url
+                if os.path.isfile(path):
+                    os.remove(path)
+        super(mod, self ).save(*args, **kwargs)
+        return
