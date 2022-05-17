e0757da8cf697e6fd94e70e84b94d209ad28b626
kkahlua <csw93083@naver.com>
2022-05-17 14:20:15 +0900

    class이름, 변수 이름 정리

--- static/jsCss/list.js
+++ static/jsCss/list.js
@@ -5,17 +5,15 @@ document.querySelector(".jsFilter").addEventListener("click", function () {
 document.querySelector(".grid").addEventListener("click", function () {
   document.querySelector(".list").classList.remove("active");
   document.querySelector(".grid").classList.add("active");
-  document.querySelector(".products-area-wrapper").classList.add("gridView");
-  document
-    .querySelector(".products-area-wrapper")
-    .classList.remove("tableView");
+  document.querySelector(".cctv-area-wrapper").classList.add("gridView");
+  document.querySelector(".cctv-area-wrapper").classList.remove("tableView");
 });
 
 document.querySelector(".list").addEventListener("click", function () {
   document.querySelector(".list").classList.add("active");
   document.querySelector(".grid").classList.remove("active");
-  document.querySelector(".products-area-wrapper").classList.remove("gridView");
-  document.querySelector(".products-area-wrapper").classList.add("tableView");
+  document.querySelector(".cctv-area-wrapper").classList.remove("gridView");
+  document.querySelector(".cctv-area-wrapper").classList.add("tableView");
 });
 
 var modeSwitch = document.querySelector(".mode-switch");
