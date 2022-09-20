86a47b1ab82a3a02b443039b7bb82d23cce50969
kkahlua <csw93083@naver.com>
2022-09-20 17:38:01 +0900

    삭제 시 media 경로의 이미지도 삭제하도록 변경

--- cctv_app/models.py
+++ cctv_app/models.py
@@ -1,3 +1,5 @@
+import os
+from django.conf import settings
 from django.db import models
 
 class Board(models.Model):
@@ -11,3 +13,8 @@ class Board(models.Model):
 
     def __str__(self):
         return str(self.title)
+    
+    def delete(self,*args, **kwargs):
+        if self.files:
+            os.remove(os.path.join(settings.MEDIA_ROOT,self.files.path))
+        super(Board,self).delete(*args, **kwargs)
